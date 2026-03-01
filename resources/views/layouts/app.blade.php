6bd228274e971eebf20a9b0166fa0843b33eb047
sonita-dev <sonitaheangpretty@gmail.com>
2026-03-01 15:29:04 +0700

    update app logo , app name and its favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
 
         <title>{{ config('app.name', 'Coupon Management System') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ asset('images/coupon_logo.png') }}">
+        <link rel="shortcut icon" href="{{ asset('images/coupon_logo.png') }}">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800&display=swap" rel="stylesheet" />
 
